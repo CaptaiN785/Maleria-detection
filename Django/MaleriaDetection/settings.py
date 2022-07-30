b798c5ac6ac0ce34ec66a972e996cc496aeb3bbc
Mukesh Kumar Thakur <83746932+CaptaiN785@users.noreply.github.com>
2022-07-30 15:38:35 +0530

    Changes some lines of codee

--- Django/MaleriaDetection/settings.py
+++ Django/MaleriaDetection/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f!1od@@m!wzhf+rkg#5_+24_w9&j$2rd0a=mc#(^fedn_u^(*#'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
